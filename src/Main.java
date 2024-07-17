eef4f0f5955847010566895dc5bf0a69c1404b4b
VolkySe <sergej_lykov@mail.ru>
2024-07-17 22:20:57 +0300

    Task1&2

--- src/Main.java
+++ src/Main.java
@@ -1,11 +1,25 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Hello world!");
-        int ChegoTam = 5;
-        switch (ChegoTam) {
-
+        System.out.println("Task1");
+        boolean clientOS = true;
+        if (clientOS) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else {
+            System.out.println("Установите версию приложения для iOS по ссылке");
         }
+
+        System.out.println("Task2");
+        byte yearOfProduct = 14;
+        boolean isProductYearBefore2015 = (yearOfProduct < 15);
+        if (isProductYearBefore2015 && clientOS) {
+            System.out.println("Установите облегченную версию приложения для Android по ссылке");
+        } else if (isProductYearBefore2015 && !clientOS) {
+            System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+        } else if (!isProductYearBefore2015 && clientOS) {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        } else if (!isProductYearBefore2015 && !clientOS) {
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
     }
-
-
 }
