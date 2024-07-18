6e0abc681c6b4e0d7b4731088aa8b9189cbc2864
VolkySe <sergej_lykov@mail.ru>
2024-07-18 22:39:20 +0300

    Task 3 Solution added

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Task1");
+
         boolean clientOS = true;
         if (clientOS) {
             System.out.println("Установите версию приложения для Android по ссылке");
@@ -9,17 +10,31 @@ public class Main {
         }
 
         System.out.println("Task2");
-        byte yearOfProduct = 14;
-        boolean isProductYearBefore2015 = (yearOfProduct < 15);
-        if (isProductYearBefore2015 && clientOS) {
+
+        byte yearOfMobile = 14;
+        boolean isYearOfMobileBefore2015 = (yearOfMobile < 15);
+        if (isYearOfMobileBefore2015 && clientOS) {
             System.out.println("Установите облегченную версию приложения для Android по ссылке");
-        } else if (isProductYearBefore2015 && !clientOS) {
+        } else if (isYearOfMobileBefore2015 && !clientOS) {
             System.out.println("Установите облегченную версию приложения для iOS по ссылке");
-        } else if (!isProductYearBefore2015 && clientOS) {
+        } else if (!isYearOfMobileBefore2015 && clientOS) {
             System.out.println("Установите версию приложения для Android по ссылке");
-        } else if (!isProductYearBefore2015 && !clientOS) {
+        } else if (!isYearOfMobileBefore2015 && !clientOS) {
             System.out.println("Установите версию приложения для iOS по ссылке");
+        } else {
+            System.out.println("Воспользуйтесь веб-версией приложения по ссылке");
         }
 
+        System.out.println("Task3");
+
+        short year = 2021;
+
+        boolean isLeapYear = ((year / 400 * 400 == year && year > 1584)
+                || (year / 4 * 4 == year && !(year / 100 * 100 == year) && year > 1584));
+        if (isLeapYear) {
+            System.out.println(year + " год является високосным");
+        } else {
+            System.out.println(year + " год не является високосным");
+        }
     }
 }
